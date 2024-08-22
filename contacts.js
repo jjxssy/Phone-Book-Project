0067ad5d3780a912f40bdcfa4a82cdff1a1880f3
jjxssy <yasminbits@gmail.com>
2024-08-22 23:36:47 +0300

    fixed search bar added responsive fixed profile picture fixed create

--- contacts.js
+++ contacts.js
@@ -5,45 +5,44 @@ let contacts = [];
 let currentColor = "#d7909bba";
 let currentColorHover = "lightpink";
 let toggleLightDark = 0;
+let defaultImg ="https://github.com/jjxssy/Phone-Book-Project/blob/main/contact-icon.png?raw=true";
 
 // an example of contacts which will display when app opens.
 let example = [
     {
-        name: "Jess", phone: "05412344567", address: "Nehser", email: "jxss@gmail.com", image: "", notes: ""
+        name: "Jess", phone: "05412344567", address: "Nehser", email: "jxss@gmail.com", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg/1024px-Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg", notes: ""
     },
     {
         name: "sam", phone: "0548765422", address: "Nesher", email: "sams@gmail.com", image: "", notes: ""
     },
     {
-        name: "nickolas", phone: "0527482915", address: "Haifa", email: "NickolasHEhe@gmail.com", notes: "moved to Haifa recently"
+        name: "nickolas", phone: "0527482915", address: "Haifa", email: "NickolasHEhe@gmail.com", image: "", notes: "moved to Haifa recently"
     },
     {
-        name: "blum", phone: "0564289571", address: "new york", email: "blum@gmail.com", notes: ""
+        name: "blum", phone: "0564289571", address: "new york", email: "blum@gmail.com", image: "", notes: ""
     }];
 contacts.push(...example);
 
 
 // this code will create a list of contacts with all specified elements.
 const ul = document.getElementById("contacts-list");
-let array = contacts.slice(0, 6);
 let content = "";
-array.forEach((elem, ind) => {
+contacts.forEach((elem, ind) => {
     const li=document.createElement('li');
     li.id = elem.phone;
     li.classList.add('contact');
-    li.innerHTML = ` <div class="photo"></div>
+    li.innerHTML = ` <img src="${(elem.image===""?defaultImg:elem.image)}" class="photo"></img>
                             <div><p class="josefin-sans info text">${elem.name}</p></div>
                             <div class="button-menu">
                                 <button
                                     class="button josefin-sans" onclick=view("${ind}")>view</button>
                                 <button
-                                    class="button josefin-sans" onclick=editCon("${elem.phone}")>edit</button>
+                                    class="button josefin-sans" onclick=editCon("${ind}")>edit</button>
                                 <button class="button josefin-sans" onclick=deleteCont("${elem.phone}")>delete</button>
                             </div>
                         `;
     ul.append(li);
     attachHover(li);
-
 });
 
 function toggleMode() { // toggling between light and dark mode
@@ -114,20 +113,28 @@ function save(e) {
     let email = document.getElementById('email').value;
     let image = document.getElementById('image');
     let notes = document.getElementById('notes').value;
-    let cont = { name: name, phone: phone, address: address, email: email, image: image !== null ? image.value : "", notes: notes };
+
+    if (!name || !phone)
+    {
+        alert("You must fill name and phone number fields in order to create a contact")
+        return;
+    }
+
+    let cont = { name: name, phone: phone, address: address, email: email, image: image !== null ? image : defaultImg, notes: notes };
     document.getElementById('name').value = "";
     document.getElementById('phone').value = "";
     document.getElementById('address').value = "";
     document.getElementById('email').value = "";
     document.getElementById('notes').value = "";
-    contacts.push(cont);
-    refresh();
-}
 
+    contacts.push(cont);
+    refresh(contacts);
+}
+// function to delete all the contacts in the phonebo
 function deleteAll() {
     if (confirm("Are you sure you want to delete all of your contacts?") === true) {
         contacts = [];
-        refresh();
+        refresh(contacts);
     }
 }
 
@@ -138,41 +145,76 @@ function deleteCont(phoneNumber) {
 }
 
 // function will build the list of contacts again.
-function refresh() {
-    ul.innerHTML = " ";
-    let content = "";
-    array= contacts.slice(current, 7);
-    array.forEach(elem => {
+function refresh(data) {
+    ul.innerHTML = "";
+    data.forEach((elem, ind) => {
         const li=document.createElement('li');
         li.id = elem.phone;
         li.classList.add('contact');
-        li.innerHTML = `<div class="photo"></div>
-                            <p class="josefin-sans info text">${elem.name}</p>
+        li.innerHTML = ` <img src="${(elem.image===""?defaultImg:elem.image)}" class="photo"></img>
+                            <div><p class="josefin-sans info text">${elem.name}</p></div>
                             <div class="button-menu">
                                 <button
-                                    class="button josefin-sans" onclick=view("${elem.phoneNumber}")>view</button>
+                                    class="button josefin-sans" onclick=view("${ind}")>view</button>
                                 <button
-                                    class="button josefin-sans" onclick=editCon("${elem.phone}")>edit</button>
+                                    class="button josefin-sans" onclick=editCon("${ind}")>edit</button>
                                 <button class="button josefin-sans" onclick=deleteCont("${elem.phone}")>delete</button>
-                            </div > `;
+                            </div>
+                        `;
     ul.append(li);
     attachHover(li);             
     });
 }
 //edit button
-function editCon(phoneNumber) { // edit the contact information pop up
-    const contact = contacts.find((contact) => contact.phone === phoneNumber);
+function editCon(index) { // edit the contact information pop up
     const popup = document.getElementById('popup1');
     const ov = document.getElementById('overlay');
     popup.classList.add('visible');
     ov.classList.add('visible');
     popup.style.display = 'flex';
-    popup.querySelector("#name1").value = contact.name;
-    popup.querySelector("#phone1").value = contact.phone;
-    popup.querySelector("#email1").value = contact.email;
-    popup.querySelector("#address1").value = contact.address;
-    popup.querySelector("#notes1").value = contact.notes;
+    popup.querySelector("#name1").value = contacts[index].name;
+    popup.querySelector("#phone1").value = contacts[index].phone;
+    popup.querySelector("#email1").value = contacts[index].email;
+    popup.querySelector("#address1").value = contacts[index].address;
+    popup.querySelector("#fileImg1").value = contacts[index].image || "";
+    popup.querySelector("#notes1").value = contacts[index].notes || "";
 }
+
+function saveEdit(e){
+    e.preventDefault();
+    const popup = document.getElementById('popup1');
+    const ov = document.getElementById('overlay');
+    let name = popup.querySelector("#name1").value;
+    let phone = popup.querySelector("#phone1").value;
+    let email = popup.querySelector("#email1").value;
+    let address = popup.querySelector("#address1").value;
+
+    if (!name || !phone || !email || !address)
+    {
+        alert("One or more of the following fields must be filled.");
+        return;
+    }
+
+    popup.classList.remove('visible');
+    ov.classList.remove('visible');
+
+    let ind = contacts.findIndex(contact => contact.phone === phone);
+    if (ind != -1)
+    {
+        contacts[ind].name = name;
+        contacts[ind].phone = phone;
+        contacts[ind].email = email;
+        contacts[ind].address = address;
+        contacts[ind].image = popup.querySelector("#fileImg1").value || "";
+        contacts[ind].notes = popup.querySelector("#notes1").value || "";
+        refresh(contacts);
+    }
+    else
+    {
+        alert(`A contact with a given phone number ${phone} doesn't exist, so you can't edit.`)   
+    }
+}
+
 function closeIX1() { // closing the edit the contact information pop up
     const pop = document.getElementById('popup1');
     const ov = document.getElementById('overlay');
@@ -181,10 +223,12 @@ function closeIX1() { // closing the edit the contact information pop up
 }
 // search bar
 function search() {
-    let input;
-    input = document.querySelector(".search"); // get the search input by class
-    const data = contacts.filter((contact)  => contact.name.toUpperCase().startsWith(input.value.toUpperCase()));
-    refresh(data);
+    let input = document.querySelector(".search"); // get the search input by class
+
+    input.addEventListener("input", (e) => {
+        const data = contacts.filter((contact)  => contact.name.toUpperCase().startsWith(e.target.value.toUpperCase()));
+        refresh(data);
+    })
 }
 
 //button view
